ebbb54aaf3fb0ebd4c0d562f9af39f09ff5e8f53
Phil <happy.melodist@gmail.com>
2021-11-15 22:09:40 -0600

    complete dynamic behaviors for next and previous buttons, DRYly

--- app.js
+++ app.js
@@ -63,3 +63,69 @@ When we click on the left button, move images to the right
 // });
 
 // prepare for the worst; pray for the best
+
+let list = document.querySelector(".list");
+let imgs = Array.from(list.children);
+// console.log(imgs);
+
+const nextBtn = document.querySelector(".btn-right");
+const prevBtn = document.querySelector(".btn-left");
+
+// getting the width of images
+const imgWidth = imgs[0].getBoundingClientRect().width;
+// console.log(imgWidth);
+
+// putting the images next to each other rather than atop one another
+function setImgPosition(img, index) {
+  img.style.left = imgWidth * index + "px";
+}
+
+imgs.forEach(setImgPosition);
+
+/*
+moveToImg function (to make the below functions DRY)
+*/
+
+const moveToImg = function (list, currentImg, targetImg) {
+  list.style.transform = "translateX(-" + targetImg.style.left + ")";
+  currentImg.classList.remove("current-img");
+  targetImg.classList.add("current-img");
+};
+
+/* 
+click the right button and move the images to the left
+*/
+
+nextBtn.addEventListener("click", () => {
+  const currentImg = list.querySelector(".current-img");
+  const nextImg = currentImg.nextElementSibling;
+  const nextIndex = imgs.findIndex((img) => {
+    img === nextImg;
+  });
+
+  moveToImg(list, currentImg, nextImg);
+  // list.style.transform = "translateX(-" + nextImg.style.left + ")";
+  // currentImg.classList.remove("current-img");
+  // nextImg.classList.add("current-img");
+});
+
+/* 
+click the left button and move the images to the right
+*/
+
+prevBtn.addEventListener("click", () => {
+  const currentImg = list.querySelector(".current-img");
+  const prevImg = currentImg.previousElementSibling;
+  const prevIndex = imgs.findIndex((img) => {
+    img === prevImg;
+  });
+
+  moveToImg(list, currentImg, prevImg);
+  // list.style.transform = "translateX(-" + prevImg.style.left + ")";
+  // currentImg.classList.remove("current-img");
+  // prevImg.classList.add("current-img");
+});
+
+/*
+
+*/
